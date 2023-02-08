a1913303f56200c608048b63f93d347943d524dc
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2023-02-08 17:36:29 -0400

    added vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })],
   base: '/meme-app/'
 })
