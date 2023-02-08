a287daea9fc4ef135d0d9630e798635e700d4e8b
Gregorio Vargas <gregoriovargasmarrero@gmail.com>
2023-02-08 17:29:13 -0400

    dark theme

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary mb-3">
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg mb-3">
         <div className="container-fluid">
             <a className="navbar-brand" href="#">Meme React App</a>
         </div>
